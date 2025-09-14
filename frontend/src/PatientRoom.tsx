7064328e7a0a8bb2178c7dd0e6d5247827e37431
rcutte <rcutte@student.42.fr>
2025-09-14 02:19:05 +0200

    feat: add svg info and simple animation
    playing with svg, added text path animation

--- frontend/src/PatientRoom.tsx
+++ frontend/src/PatientRoom.tsx
@@ -15,14 +15,46 @@ export const PatientRoom = () => {
     <svg width="500" height="500">
       <rect
         className={`room ${status}`}
-        x="50"
-        y="50"
-        width="250"
-        height="250"
+        x="5"
+        y="5"
+        width="490"
+        height="490"
         rx="10"
         ry="10"
       />
-      <foreignObject x="150" y="150" width="100" height="100">
+      <path
+        id="textCurve"
+        d="
+          M 50, 150 
+          C 200, 100 300, 50
+          450, 100
+          "
+        fill="transparent"
+        // stroke="red"
+        strokeWidth={2}
+      />
+      <text
+        fontSize={45}
+        fontWeight={900}
+        letterSpacing={-6}
+        fill="white"
+        stroke="black"
+        strokeWidth={3}
+        paintOrder="stroke fill markers"
+        dominantBaseline="middle"
+        className="roomText"
+      >
+        <textPath href="#textCurve" startOffset="20%">
+          <animate
+            attributeName="startOffset"
+            values="-50%;100%;"
+            dur="8s"
+            repeatCount="indefinite"
+          />
+          Claude ARGO
+        </textPath>
+      </text>
+      <foreignObject x="200" y="200" width="100" height="100">
         <div
           style={{
             display: "flex",
